5c49140505bd5277f8a8400da226ee06d68eddad
haidang91 <haiduc.dang@avenga.com>
2021-01-12 22:22:11 +0100

    update brunettes.sh

--- brunettes.sh
+++ brunettes.sh
@@ -5,4 +5,4 @@ docker run -ti -d --cap-add=NET_ADMIN --device /dev/net/tun --name vpn2 -p 4445:
 
 # start docker 
 # docker run  -it  -d -p 4445:4444 -p 5901:5900 --name insta  -v /Users/Hai/github/python/Brunettes:/home/seluser/Brunettes instabot 
-docker run  -it  -d --name brunette --net=container:vpn2  -v $(pwd)"/Brunettes":/home/seluser/Brunettes instabot 
+docker run  -it  -d --name brunette --net=container:vpn2  -v $(pwd)"/Brunettes":/home/seluser/Brunettes haidedang/instabot:latest 
